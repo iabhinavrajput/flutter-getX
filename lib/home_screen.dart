f37060422694a9fc7b741cbfaa9397aec9b9e8b3
iabhinavrajput <abhinavrajput207@gmail.com>
2025-11-04 18:50:19 +0530

    refactor: integrate GetX for state management and snackbar functionality

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,12 +11,19 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text('GetX'),
-      ),
-      body: Center(
-        child: Text('Welcome to the Home Screen!'),
+    return Scaffold(
+      appBar: AppBar(title: Text('GetX')),
+      body: Center(child: Text('Welcome to the Home Screen!')),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.message),
+        onPressed: () {
+          Get.snackbar(
+            'Hello',
+            'This is a GetX Snackbar',
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.blue[200],
+          );
+        },
       ),
     );
   }
